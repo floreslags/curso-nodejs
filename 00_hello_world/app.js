21b70a49a09632edc11b3c7778e799902e65590c
sergioflags <sergiofol1093@gmail.com>
2023-09-13 01:08:07 -0600

    00-hello-world

---
+++ 00_hello_world/app.js
@@ -0,0 +1,15 @@
+let name = 'Sergio';
+
+console.log(`Hello world, my name is ${name}. This is my first message from node js`);
+
+let age = 29;
+
+if(age>=18){
+    console.log('Eres mayor de edad');
+}else{
+    console.log('Eres menor de edad');
+}
+
+const add = (a,b) => a+b;
+console.log(add(20,44));
+
